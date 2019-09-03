de94f144c7bdb1dea9e5ece8d8d5b1ced4e5f6a8
FutureApp <Michael Czaja>
2019-09-03 22:49:15 +0200

    code-style; Improve hive-test file

--- queries/test/hive/run_test_word_count.sh
+++ queries/test/hive/run_test_word_count.sh
@@ -2,6 +2,9 @@
 
 # Upload, run and print the result
 # The validation must be performed manuelly.
+
+hadoop fs -mkdir /user/inputs/
+hadoop fs -mkdir /user/outputs/
 hadoop dfs -copyFromLocal simple-test-data.txt /user/inputs/
 hive -f word_count.hql
 hadoop fs -cat /user/outputs/000000_0
